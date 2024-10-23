17ea890327271e2f36a688039c2f3911c4ba65ca
julia <alara@alarabread.fun>
2024-10-22 23:52:25 -0400

    dynamic server target

--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
 import grpc from "@grpc/grpc-js";
 import core from "./core-implementation/core.js";
 
-export const target = "0.0.0.0:50051";
+export let target = "0.0.0.0:50051";
 export const server = new grpc.Server();
 core(server);
-server.bindAsync(
-	target,
-	grpc.ServerCredentials.createInsecure(),
-	(err, port) => {
-		if (err != null) {
-			return console.error(err);
-		}
-		console.log(`grpc listening on port ${port}`);
-	},
-);
+function tryBind() {
+	server.bindAsync(
+		target,
+		grpc.ServerCredentials.createInsecure(),
+		(err, port) => {
+			if (err != null) {
+				if (err.message.includes("address already in use")) {
+					const oldTarget = target;
+					target =
+						"0.0.0.0:" + Math.floor(Math.random() * 20000 + 10000);
+					console.log(
+						`failed to bind to ${oldTarget}\nAttempting to bind to ${target}`,
+					);
+					tryBind();
+				} else {
+					return console.error(err);
+				}
+			}
+			console.log(`grpc listening on port ${port}`);
+		},
+	);
+}
+
+tryBind();
