bd909ada6336830169054fd29483c5651949ed7e
dylandunsheath <dtd35@njit.edu>
2024-10-29 15:11:43 -0400

    Updated with comments

--- core-implementation/search_transaction_by_user/search_transaction_by_user.ts
+++ core-implementation/search_transaction_by_user/search_transaction_by_user.ts
@@ -1,31 +1,36 @@
+/*
+
+Source for comments: https://chatgpt.com/share/67213342-70c0-8003-91ef-8b6c29b25934
+
+*/
+
+
 import { handleUnaryCall } from "@grpc/grpc-js";
 import { core } from "../../generated/core/core.js";
 
-// Populated with transaction data
+// Populated with transaction data - This acts as an in-memory database for transaction records.
 export const ledger: core.Transaction[] = [];
 
-// TODO: Get Data from database
+// TODO: Integrate with a real database to retrieve data instead of using the in-memory ledger.
 export const searchTransactionByUser: handleUnaryCall<
     core.UserId, 
     core.TransactionList
   > = (request, respond) => {
     
+    // Extract the user ID from the incoming request.
     const userID = request.request.user_id;
     
-    
-    // Gets user ID which is a sender or receiever
-      // Code that matches user id of sender and receiever
-      const userTransactions = ledger.filter(
-        (transaction) =>
-          transaction.sender === userID || transaction.receiver === userID,
-      );
+    // Filter transactions by user ID - retrieves transactions where the user is either the sender or receiver.
+    const userTransactions = ledger.filter(
+      (transaction) =>
+        transaction.sender === userID || transaction.receiver === userID,
+    );
 
-      // Construct a transaction:List to respond with
-      const TransactionList = new core.TransactionList({
-        list: userTransactions,
-      });
+    // Create a TransactionList response object with the filtered transactions.
+    const TransactionList = new core.TransactionList({
+      list: userTransactions,
+    });
 
-      respond(null, TransactionList);
-    
+    // Send the response back to the client with the TransactionList data.
+    respond(null, TransactionList);
 };
-
