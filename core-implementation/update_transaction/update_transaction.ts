5f51fa9bf0c44eff3a8f3de2578d2b27735fdc3e
ags54 <ags54@njit.edu>
2024-11-02 12:21:44 -0400

    Prettier formatted files

--- core-implementation/update_transaction/update_transaction.ts
+++ core-implementation/update_transaction/update_transaction.ts
@@ -4,47 +4,43 @@ import { ledger } from "../search_transaction_by_user/search_transaction_by_user
 
 // Define handler for updating a transaction based on transaction ID
 export const updateTransaction: handleUnaryCall<
-    core.TransactionUpdate,
-    core.Status
-> = (request, respond) =>
-{
-    // Extract transaction details from the request
-    const { transaction_id, sender, receiver, amount } = request.request;
+	core.TransactionUpdate,
+	core.Status
+> = (request, respond) => {
+	// Extract transaction details from the request
+	const { transaction_id, sender, receiver, amount } = request.request;
 
-    // Find the transaction in the ledger
-    const transaction = ledger.find((tx) => tx.transaction_id === transaction_id);
+	// Find the transaction in the ledger
+	const transaction = ledger.find(
+		(tx) => tx.transaction_id === transaction_id,
+	);
 
-    if (!transaction)
-    {
-        // Transaction not found; respond with failure status
-        return respond(
-            null, 
-            new core.Status(
-            { 
-                succeeded: false 
-            }));
-    }
+	if (!transaction) {
+		// Transaction not found; respond with failure status
+		return respond(
+			null,
+			new core.Status({
+				succeeded: false,
+			}),
+		);
+	}
 
-    // Update transaction details
-    if (sender != undefined)
-    {
-        transaction.sender = sender;
-    }
-    if (receiver != undefined)
-    {
-        transaction.receiver = receiver;
-    }
-    if (amount != undefined)
-    {
-        transaction.amount = amount;
-    }
+	// Update transaction details
+	if (sender != undefined) {
+		transaction.sender = sender;
+	}
+	if (receiver != undefined) {
+		transaction.receiver = receiver;
+	}
+	if (amount != undefined) {
+		transaction.amount = amount;
+	}
 
-    // Respond with success status
-    respond(
-        null,
-        new core.Status(
-        {
-            succeeded: true,
-        })
-    )
+	// Respond with success status
+	respond(
+		null,
+		new core.Status({
+			succeeded: true,
+		}),
+	);
 };
